f32f2f3bad93a6d05ac71e3e90290fc8f0c6140e
Slava <vgrigorash@devsterlab.com>
2019-04-03 18:22:59 +0300

    configure client and implement router

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Header from './componnents/layout/Header';
+import Landing from './componnents/layout/Landing';
+import Register from './componnents/auth/Register';
+import Login from './componnents/auth/Login';
+import Footer from './componnents/layout/Footer';
+
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        project
-      </div>
+      <Router>
+        <div className="App">
+          <Header />
+          <Route exact
+                 path="/"
+                 component={Landing} />
+          <div className="container">
+            <Route exact
+                   path="/login"
+                   component={Login} />
+            <Route exact
+                   path="/register"
+                   component={Register} />
+          </div>
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
---
+++ client/src/componnents/layout/Footer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Footer = () => <footer className="bg-dark text-white p-3 text-center">
+  Copyright &copy; {new Date().getFullYear()} DevConnector
+</footer>;
+
+export default Footer;
