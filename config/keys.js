019433eaf9e895b7c6d3814a7122fa4aba51a6c3
Slava <vgrigorash@devsterlab.com>
2019-05-07 19:57:12 +0300

    git ignore

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-	mongoURI: 'mongodb+srv://buynyj:slavablyat10@devconnector-cluster-wpvc9.mongodb.net/test?retryWrites=true',
-	secretOrKey: 'secret'
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
